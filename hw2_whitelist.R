cdedecdb350316a570098d0f8c25dac3a42b59fb
Colin Rundel <rundel@gmail.com>
2018-02-06 08:20:11 -0500

    Styling  3  files:  hw1_whitelist.R ✔  hw2_whitelist.R ✔  hw1.Rmd         ✔ ──────────────────────────────────────── Status	Count	Legend ✔ 	3	File unchanged. ℹ 	0	File changed. ✖ 	0	Styling threw an eror. ────────────────────────────────────────

--- hw2_whitelist.R
+++ hw2_whitelist.R
@@ -1,24 +1,25 @@
 library(stringr)
 library(purrr)
 
-allowed_files = c("hw2.Rmd",
-                  "hw2.Rproj",
-                  "hw2_whitelist.R",
-                  "README.md",
-                  "wercker.yml",
-                  "lego_sales.rds",
-                  ".gitignore")
+allowed_files <- c(
+  "hw2.Rmd",
+  "hw2.Rproj",
+  "hw2_whitelist.R",
+  "README.md",
+  "wercker.yml",
+  "lego_sales.rds",
+  ".gitignore"
+)
 
-files = dir()
-disallowed_files = !map_lgl(files, ~ any(str_detect(., allowed_files)))
+files <- dir()
+disallowed_files <- !map_lgl(files, ~ any(str_detect(., allowed_files)))
 
-if (any(disallowed_files))
-{
+if (any(disallowed_files)) {
   cat("Disallowed files found:\n")
   cat("  (remove the following files from your repo)\n\n")
 
-  for(file in files[disallowed_files])
-    cat("*",file,"\n")
+  for (file in files[disallowed_files])
+    cat("*", file, "\n")
 
-  quit("no",1,FALSE)
+  quit("no", 1, FALSE)
 }
